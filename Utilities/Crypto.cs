d5a73590b57473c348faaad4c35110ecc6d87c95
srijanasrijana <srijanabasnet66@gmail.com>
2022-10-07 13:54:39 +0400

    Minor Changes

--- Utilities/Crypto.cs
+++ Utilities/Crypto.cs
@@ -10,13 +10,18 @@ namespace UserRegistrationTask.Utilities
         {
             if (!String.IsNullOrEmpty(textToBeEncrypted))
             {
+                //To create object of MD5
                 MD5 md5 = MD5.Create();
+                //get bytes for original password and compute hash(encoded password)
                 byte[] bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(textToBeEncrypted);
                 byte[] hash = md5.ComputeHash(bytes);
 
+                //To convert byte to string
                 StringBuilder sbPassword = new StringBuilder();
                 for (int b = 0; b < hash.Length; b++)
                 {
+                    //change it into 2 hexadecimal digits  
+                    //for each byte 
                     sbPassword.Append(hash[b].ToString("X2"));
                 }
 
@@ -29,19 +34,19 @@ namespace UserRegistrationTask.Utilities
             }
         }
 
-        public static byte[] dataURL2byte(string dataurl)
-        {
-            string[] dataURL = dataurl.Split(new char[] { ',' }, 2);
+        //public static byte[] dataURL2byte(string dataurl)
+       // {
+           // string[] dataURL = dataurl.Split(new char[] { ',' }, 2);
 
-            byte[] urlByte = Convert.FromBase64String(dataURL[1]);
-            return urlByte;
-        }
+           // byte[] urlByte = Convert.FromBase64String(dataURL[1]);
+            //return urlByte;
+       // }
 
-        public static string byte2dataURL(byte[] urlByte)
-        {
-            string dataURL = Convert.ToBase64String(urlByte);
-            return dataURL;
-        }
+        //public static string byte2dataURL(byte[] urlByte)
+       // {
+          //  string dataURL = Convert.ToBase64String(urlByte);
+          //  return dataURL;
+        //}
     }
 }
 
